bb6ae50fce13ec88b80589b671cc06b130c30b14
TseYi <airder4@yahoo.com.tw>
2018-12-22 18:00:13 +0900

    20181222 last

--- demo_app/urls.py
+++ demo_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('input_form', views.input_form, name='input_form'),
     path('result', views.result, name='result'),
-    path('history', views.history, name='history'), 
+    path('history', views.history, name='history'),
+    path('signup', views.signup, name='signup'),
+    path('info', views.info, name='info'),
 ]
--- demo_app/views.py
+++ demo_app/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render, redirect
-from .forms import InputForm
+from .forms import InputForm, SignUpForm
 from .models import Customer
 from sklearn.externals import joblib
 import numpy as np
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+import pandas as pd
+import json
 
 # モデルの呼び出しはグローバル変数のところで書くことがベター
-loaded_model = joblib.load('/home/airder/airder.pythonanywhere.com/demo_app/demo_model.pkl')
+#loaded_model = joblib.load('demo_app/demo_model.pkl') #ローカル用
+loaded_model = joblib.load('/home/airder/airder.pythonanywhere.com/demo_app/demo_model.pkl') #pythonanywhere用
 
+#@login_required
 def index(request):
     return render(request, 'demo_app/index.html', {})
     # {}はhtml側に渡す変数
 
+
+#@login_required
 def input_form(request):
     if request.method == "POST":
         form = InputForm(request.POST)
@@ -21,6 +29,7 @@ def input_form(request):
         form = InputForm()
         return render(request, 'demo_app/input_form.html', {'form':form})
 
+#@login_required
 def result(request):
     #DBからデータを取得
     _data = Customer.objects.order_by('id').reverse().values_list\
@@ -58,6 +67,7 @@ def result(request):
 
     return render(request, 'demo_app/result.html', {'y':y[0], 'y_proba':round(_y_proba[0][y[0]], 2), 'comment':comment})
 
+#@login_required
 def history(request):
     if request.method == 'POST':
         d_id = request.POST
@@ -68,3 +78,49 @@ def history(request):
     else:
         customers = Customer.objects.all()
         return render(request, 'demo_app/history.html', {'customers':customers})
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = SignUpForm()
+    return render(request, 'demo_app/signup.html', {'form': form})
+
+def info(request):
+    # DBからデータの読み込み
+    customers = Customer.objects.values_list(\
+    'gender', 'education', 'marriage', 'age', 'result', 'proba')
+
+    # データをDataFarame型に変換
+    lis, cols = [], ['gender', 'education', 'marriage', 'age', 'result', 'proba']
+    for customer in customers:
+        lis.append(customer)
+    df = pd.DataFrame(lis, columns=cols)
+
+    # データの整形
+    df['gender'].replace({1:"男性", 2:"女性"}, inplace=True)
+    df['education'].replace({1:'graduate_school', 2:'university', 3:'high school', 4:'other'}, inplace=True)
+    df['marriage'].replace({1:'married', 2:'single', 3:'others'}, inplace=True)
+    df['result'].replace({0:'審査落ち', 1:'審査通過', 2:'その他'}, inplace=True)
+    df['age'] = pd.cut(df['age'], [0,10,20,30,40,50,60,100], labels=['10歳以下', '10代','20代','30代','40代','50代','60代以上'])
+    df['proba'] = pd.cut(df['proba'], [0,0.75,1], labels=['要審査', '信頼度高'])
+
+    # データのユニークな値とその数の取得
+    dic_val, dic_index = {}, {}
+    for col in cols:
+        _val = df[col].value_counts().tolist()
+        _index = df[col].value_counts().index.tolist()
+        dic_val[col] = _val
+        dic_index[col] = _index
+
+    # データをJson形式に変換
+    val, index = json.dumps(dic_val), json.dumps(dic_index)
+
+    return render(request, 'demo_app/info.html', {'index':index, 'val':val})
